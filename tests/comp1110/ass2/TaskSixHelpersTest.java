5b7b2f0708babd2bb5918b3e1562bf2e7c4d0b79
Luo Xiangyi <luoxiangyi222@gmail.com>
2017-09-26 02:07:43 +1000

    pull from master

---
+++ tests/comp1110/ass2/TaskSixHelpersTest.java
@@ -0,0 +1,28 @@
+package comp1110.ass2;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+/**
+ * Created by luoxi on 26/09/2017.
+ */
+public class TaskSixHelpersTest {
+
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(1000);
+
+    @Test
+    public void testGetCandidates(){
+
+    }
+
+    @Test
+    public void testValidOrders(){
+
+    }
+
+    @Test
+    public void testNotObstruct(){
+
+    }
+}
