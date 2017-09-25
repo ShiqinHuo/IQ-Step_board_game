5513d6285f3ce8db8442fa0d8d39047ce27db397
ShiqinHuo <ShiqinHuo@gmail.com>
2017-09-25 21:27:51 +1000

    testGoodPeg/BadPeg/RandomPos fixed!

--- tests/comp1110/ass2/isPegTest.java
+++ tests/comp1110/ass2/isPegTest.java
@@ -7,6 +7,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import static comp1110.ass2.Alphabet.isPeg;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class isPegTest {
@@ -17,14 +18,41 @@ public class isPegTest {
 
     private static int[] pegset = {0, 2, 4, 6, 8, 11, 13, 15, 17, 19, 20, 22, 24,
             26, 28, 31, 33, 35, 37, 39, 40, 42, 44, 46, 48};
+
+    @Test
+    public void testGoodPeg(){
+        int pos1 = 0;
+        int pos2 = 19;
+        int pos3 = 40;
+        assertTrue("testGoodPeg1",isPeg(pos1));
+        assertTrue("testGoodPeg2",isPeg(pos2));
+        assertTrue("testGoodPeg3",isPeg(pos3));
+    }
+    @Test
+    public void testBadPeg(){
+        int pos1 = 5;
+        int pos2 = 18;
+        int pos3 = 23;
+        assertFalse("testBadPeg1",isPeg(pos1));
+        assertFalse("testBadPeg2",isPeg(pos2));
+        assertFalse("testBadPeg3",isPeg(pos3));
+    }
 //https://stackoverflow.com/questions/363681/how-do-i-generate-random-integers-within-a-specific-range-in-java
     int randomNum = 0 + (int)(Math.random() * 49);
     @Test
-    public void testPeg() {
-            for (int j = 0; j<pegset.length;j++){
-                if (randomNum == j)
-                    ispeg = true;}
-            assertTrue(ispeg==isPeg(randomNum));
+    public void testRandomPos() {
+        for (int j = 0; j<pegset.length;j++){
+            if (randomNum == pegset[j])
+            {
+                ispeg = true;
+                //System.out.println("index"+j);
+                break;}
         }
-    }   
+        //System.out.println(randomNum);
+        //System.out.println(ispeg);
+        assertTrue(ispeg==isPeg(randomNum));
+        }
+
+    }
+
 
